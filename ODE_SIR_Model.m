5c10e226b833f983a84accca4d8633de26d35f72
ilmissigno <48990987+ilmissigno@users.noreply.github.com>
2019-05-06 08:45:50 +0200

    Elaborato 6

---
+++ ODE_SIR_Model.m
@@ -0,0 +1,26 @@
+%% Funzione del modello ODE SIR di diffusione di un Epidemia
+function y_primo = ODE_SIR_Model(t,y_iniziale,a)
+
+%% MODELLO ODE
+
+%{
+S'(t) = -aS(t)I(t)
+I'(t) = aS(t)I(t) - bI(t)
+R'(t) = bI(t)
+con t ? [0,20] , S(0) = 199 , I(0) = 1 , R(0) = 0
+b costante = 0.1
+
+Nella seguente notazione, il matlab specifica che
+per una variabile la notazione (i,j) indica la i 
+equazione nella j incognita.
+Di conseguenza
+y'(prima equaz,prima incog) = -a*y(prima incog)*y(sec incog)
+ecc.. variando la i per indicare il numero di equazioni
+%}
+
+b=0.1;
+y_primo(1,1) = -a*y_iniziale(1)*y_iniziale(2);
+y_primo(2,1) = a*y_iniziale(1)*y_iniziale(2) - b*y_iniziale(2);
+y_primo(3,1) = b*y_iniziale(2);
+
+end
---
+++ Risolve_SIR.m
@@ -0,0 +1,32 @@
+%% Script di Risoluzione del Modello SIR
+function Risolve_SIR(a)
+%Impostazione condizioni iniziali
+t = [0 20];
+s0 = 199;
+i0 = 1;
+r0 = 0;
+%Impostazione valori di tolleranza dell'errore
+opzione = odeset('RelTol',10^-6,'AbsTol',10^-7);
+%Calcolo del modello attraverso ode45
+[time,result] = ode45(@ODE_SIR_Model,t,[s0 i0 r0],opzione,a);
+%Calcolo Picco degli Infetti
+peak = max(result(:,2));
+time_peak = time(result(:,2)==peak)
+%Grafico del Modello
+subplot(2,1,1);
+p = plot(time,result(:,1),'r',time,result(:,2),'g',time,result(:,3),'b',time_peak,peak,'x','MarkerSize',10,'LineWidth',2);
+title('Modello SIR di diffusione di un''epidemia');
+xlabel('Tempo');
+ylabel('Misurazioni');
+legend('Persone Suscettibili','Infetti','Immuni','Picco degli Infetti');
+%Grafico della Percentuale di infetti
+%La percentuale degli infetti si calcola come:
+%(infetti/(suscettibili+guariti))*100
+subplot(2,1,2);
+plot(time,(result(:,2)/(result(:,1)+result(:,3)))*100,'m--');
+title('Percentuale degli Individui infetti');
+xlabel('Tempo');
+ylabel('Percentuale');
+%definisco dei limiti per gli assi
+axis([0 20 0 100]);
+end
