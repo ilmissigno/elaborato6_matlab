6c14041e04dfd94c78734d548093d26bfaa687df
Giuseppe Napolano <peppenapo@live.it>
2019-05-06 09:01:40 +0200

    aa

--- Oregonator_ODE.m
+++ Oregonator_ODE.m
@@ -2,7 +2,7 @@
 function y_primo = Oregonator_ODE(t,y,q,e,g,f)
 
 %% MODELLO ODE
-
+%%
 %{
 y1'(t) = (qy2 - y1y2 + y1(1-y1))/e
 y2'(t) = (-qy2 - y1y2 + fy3)/g
@@ -11,7 +11,7 @@ con t ? [0,50] , y1(0) = y2(0) = y3(0) = 0.2
 q = 9x10^-5 , e = 10^-2 , g = 2.5x10^-5 , f=0.8
 
 Nella seguente notazione, il matlab specifica che
-per una variabile la notazione (i,j) indica la i 
+per una variabile la notazione (i,j) indica la i
 equazione nella j incognita.
 Di conseguenza
 y1'(prima equaz,prima incog) = q*y(sec incog) - y(prima incog)*y(sec incog)
